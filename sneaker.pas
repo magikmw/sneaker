8db3f7a986792f5bb47f656dd0c46cd2eac51be4
Michał Walczak <mjf.walczak@gmail.com>
2012-05-13 02:41:47 +0200

    binary to octal done, output framework working

--- sneaker.pas
+++ sneaker.pas
@@ -41,18 +41,96 @@ oct:STRING;
 dec:STRING;
 hex:STRING;
 
+octbin:array[0..7] of STRING;
+
 {Procedures and functions declarations.}
+
+Procedure octbintablefill;
+	Begin
+		{set the table}
+		octbin[0]:='000';
+		octbin[1]:='001';
+		octbin[2]:='010';
+		octbin[3]:='011';
+		octbin[4]:='100';
+		octbin[5]:='101';
+		octbin[6]:='110';
+		octbin[7]:='111';
+	end;
+
+Function reverse(s:string):string; {stolen from rosettacode}
+var i:integer;
+var tmp:char;
+begin
+    for i:=1 to length(s) div 2 do
+      begin
+       tmp:=s[i];
+       s[i]:=s[length(s)+1-i];
+       s[length(s)+1-i]:=tmp;
+       reverse:=s;
+      end;
+end;
+
 Procedure binary(input:STRING);
 	var
-	nbin:LONGINT;
-	noct:LONGINT;
-	ndoc:LONGINT;
-	nhex:STRING;
-Begin
+	tmpstr:STRING;
+	counter:INTEGER;
+	x:INTEGER;
+	i:INTEGER;
+	ii:INTEGER;
+	Begin
+
+		{first bin to oct}
+		if byte(input[0]) mod 3 = 0 then
+		begin
+			counter:=byte(input[0]) div 3;
+			writeln('Counter is = ', counter);
+
+			for i:=0 to counter-1 do
+			begin
+				x:=3*i;
+
+				if i > 0 then
+				begin
+				tmpstr:=Copy(input, x+1, 3);
+				ii:=0;
+
+					while ii <> 8 do
+					begin
+						if tmpstr = octbin[ii] then
+						begin
+							Str(ii, tmpstr);
+							oct:=Concat(oct,tmpstr);
+							ii:=8;
+						end
+						else ii:=ii+1;
+					end;
+				writeln(oct);
+				end
+
+				else begin
+				tmpstr:=Copy(input, i+1, 3);
+				ii:=0;
+
+				while ii <> 8 do
+				begin
+					if tmpstr = octbin[ii] then
+					begin
+						Str(ii, tmpstr);
+						oct:=Concat(oct,tmpstr);
+						ii:=8;
+					end
+					else ii:=ii+1;
+				end;
+				writeln(oct);
+				end;
+			end;
+		end;
+
+	writeln('Input lenght = ',byte(input[0]));
 
 
-
-end;
+	end;
 
 Procedure octal(input:STRING);
 	var
@@ -60,11 +138,11 @@ Procedure octal(input:STRING);
 	noct:LONGINT;
 	ndoc:LONGINT;
 	nhex:STRING;
-Begin
+	Begin
 
 
 
-end;
+	end;
 
 Procedure decimal(input:STRING);
 	var
@@ -72,11 +150,11 @@ Procedure decimal(input:STRING);
 	noct:LONGINT;
 	ndoc:LONGINT;
 	nhex:STRING;
-Begin
+	Begin
 
 
 
-end;
+	end;
 
 Procedure hexadecimal(input:STRING);
 	var
@@ -84,49 +162,58 @@ Procedure hexadecimal(input:STRING);
 	noct:LONGINT;
 	ndoc:LONGINT;
 	nhex:STRING;
-Begin
+	Begin
 
 
 
-end;
+	end;
 
 Begin {Program begin}
 
-if ParamCount <> 2 then begin
-	writeln('Please specify the numeric system and the number - i.e. sneaker bin 101010');
-	exit;
-end
-else begin
-	numsysstr:=ParamStr(1);
-	converteestr:=ParamStr(2);
-	{val(numsysstr, numsys);
-	val(converteestr, convertee);}
-end;
+	octbintablefill;
 
-if numsysstr = 'bin' then
-	begin
-		bin:=converteestr;
-		binary(bin);
+	if ParamCount <> 2 then begin
+		writeln('Please specify the numeric system and the number - i.e. sneaker bin 101010');
+		exit;
 	end
-else if numsysstr = 'oct' then
-	begin
-		oct:=converteestr;
-		octal(oct);
-		writeln('Not yet implemented.')
-	end
-else if numsysstr = 'dec' then
-	begin
-		dec:=converteestr;
-		decimal(dec);
-		writeln('Not yet implemented.')
-	end
-else if numsysstr = 'hex' then
-	begin
-		hex:=converteestr;
-		hexadecimal(hex);
-		writeln('Not yet implemented.')
+	else begin
+		numsysstr:=ParamStr(1);
+		converteestr:=ParamStr(2);
+		{val(numsysstr, numsys);
+		val(converteestr, convertee);}
 	end;
 
-writeln(converteestr)
+	if numsysstr = 'bin' then
+		begin
+			bin:=converteestr;
+			binary(bin);
+			writeln('You requested conversion for binary ', converteestr, '.');
+		end
+	else if numsysstr = 'oct' then
+		begin
+			oct:=converteestr;
+			octal(oct);
+			writeln('You requested conversion for octal ', converteestr, '.');
+			writeln('Not yet implemented.')
+		end
+	else if numsysstr = 'dec' then
+		begin
+			dec:=converteestr;
+			decimal(dec);
+			writeln('You requested conversion for decimal ', converteestr, '.');
+			writeln('Not yet implemented.')
+		end
+	else if numsysstr = 'hex' then
+		begin
+			hex:=converteestr;
+			hexadecimal(hex);
+			writeln('You requested conversion for hexadecimal ', converteestr, '.');
+			writeln('Not yet implemented.')
+		end;
+
+	writeln('Binary:      ', bin);
+	writeln('Octal:       ', oct);
+	writeln('Decimal:     ', dec);
+	writeln('Hexacedimal: ', hex);
 
 End. {Program end}
